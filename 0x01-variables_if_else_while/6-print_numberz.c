91939d0a7807254db81fd43884fb25a14387c165
Aylinkup <www.fxace@gmail.com>
2022-09-09 19:03:49 -0700

    C program

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+/**
+ * main - prints all single digit numbers of base 10 starting from 06
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 48;
+	while  (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
