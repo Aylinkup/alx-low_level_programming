97278946e547e18238c62781011d5aae350f9dd1
Aylinkup <www.fxace@gmail.com>
2022-09-13 12:44:02 -0700

    C program - Functions, nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,10 @@
 #include "main.h"
+
 /**
-* main - check the code.
-*
-* Return: Always 0
-*/
+ * _isalpha - checks the code for Holberton School students.
+ * 
+ * Return: 0
+ */
 int _isalpha(int c)
 {
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
