772a8bd55222d0a3baea9fb4b45da68076978b44
Aylinkup <www.fxace@gmail.com>
2022-09-13 15:34:02 -0700

    C program - Functions, nested loops

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "holberton.h"
+#include <maim.h>
+
 /**
  * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
  *        separated by a comma followed by a space.
  *
- * Return: 0
+ * Return: Always 0.
  */
 int main(void)
 {
