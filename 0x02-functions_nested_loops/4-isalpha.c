c6a109a25a8886dc04c04e59918f3d3c56879401
Aylinkup <www.fxace@gmail.com>
2022-09-13 12:39:30 -0700

    C program - Functions, nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,18 @@
 #include "main.h"
-
 /**
- * _isalpha - checks for alphebetic characters.
- * 
- * Return: 0
- */
-
+* main - check the code.
+*
+* Return: Always 0
+*/
 int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-		return (1);
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+	return (1);
+	}
 	else
-		return (0);
+	{
+	return (0);
+	}
+	_putchar('\n');
 }
